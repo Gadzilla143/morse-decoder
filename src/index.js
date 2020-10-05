731413d1aeda3bcb18612868f8daf84716cec768
gadzilla143 <egor1231232@gmail.com>
2020-10-05 16:29:18 +0300

    init: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let morse_str = '';
+    let morse_arr = [];
+    for (let i = 0; i < expr.length/10; i++){
+        morse_arr[i] = expr.substring(i*10, i*10 +10)
+    }
+    for (let i = 0; i < morse_arr.length; i++){
+        let morse_symbol_str = '';
+        for (let k = 0; k < morse_arr[i].length; k++){
+            
+            if(morse_arr[i][k] == '1'){
+                k++
+                if (morse_arr[i][k] == '1'){
+                    morse_symbol_str += '-';
+                } else if (morse_arr[i][k] == '0') {
+                    morse_symbol_str += '.';
+                }
+            } else if (morse_arr[i][k] == '*'){
+                morse_symbol_str = ' ';
+            }
+        }
+        if (morse_symbol_str == ' '){
+            morse_str += ' ';
+        } else {
+            morse_str += MORSE_TABLE[morse_symbol_str];
+        }
+    }
+    return morse_str;
 }
 
 module.exports = {
